28d9de16f20646e00590061f6014c344360a372f
Adam Hegedus <Adam.Hegedus@EnergyCAP.com>
2016-10-17 14:07:30 -0400

    Added new alert modal added all ui views & classes

---
+++ Access/TicketsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TicketsTableViewCell.swift
+//  Access
+//
+//  Created by Adam Hegedus on 10/17/16.
+//  Copyright © 2016 EnergyCAP, Inc. All rights reserved.
+//
+
+import UIKit
+
+class TicketsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
